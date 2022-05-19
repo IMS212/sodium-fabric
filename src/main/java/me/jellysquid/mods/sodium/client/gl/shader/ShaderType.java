da517df1eea4d07a93f9d47f68d76697a1f67504
IMS212 <ims@ims21.tech>
2022-05-19 13:41:19 -0700

    Add other shader types

--- src/main/java/me/jellysquid/mods/sodium/client/gl/shader/ShaderType.java
+++ src/main/java/me/jellysquid/mods/sodium/client/gl/shader/ShaderType.java
@@ -1,13 +1,17 @@
 package me.jellysquid.mods.sodium.client.gl.shader;
 
 import org.lwjgl.opengl.GL20C;
+import org.lwjgl.opengl.GL32C;
+import org.lwjgl.opengl.GL43C;
 
 /**
  * An enumeration over the supported OpenGL shader types.
  */
 public enum ShaderType {
     VERTEX(GL20C.GL_VERTEX_SHADER),
-    FRAGMENT(GL20C.GL_FRAGMENT_SHADER);
+    FRAGMENT(GL20C.GL_FRAGMENT_SHADER),
+    GEOMETRY(GL32C.GL_GEOMETRY_SHADER),
+    COMPUTE(GL43C.GL_COMPUTE_SHADER);
 
     public final int id;
 
