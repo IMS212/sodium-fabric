f75df1e11b0e6b32488793a397f608915f7fd6e6
IMS212 <ims@ims21.tech>
2022-11-06 13:34:31 -0800

    fix build

--- src/main/java/net/caffeinemc/sodium/render/terrain/format/AccelerationSink.java
+++ src/main/java/net/caffeinemc/sodium/render/terrain/format/AccelerationSink.java
@@ -2,7 +2,6 @@ package net.caffeinemc.sodium.render.terrain.format;
 
 public interface AccelerationSink {
     AccelerationBufferSink getAccelerationBuffer();
-    void putLightInBuffer(int block, float x, float y, float z);
     void writeAccelerationVertex(float posX, float posY, float posZ);
 
     void writeMeta(float a, float b);
