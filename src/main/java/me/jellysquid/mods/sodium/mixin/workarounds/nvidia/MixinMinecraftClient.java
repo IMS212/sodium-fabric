93f0689d44a66db86efe7b5175f9c564529972e0
IMS212 <ims@irisshaders.dev>
2023-01-01 11:42:18 -0800

    new: What the fuck?

--- src/main/java/me/jellysquid/mods/sodium/mixin/workarounds/nvidia/MixinMinecraftClient.java
+++ src/main/java/me/jellysquid/mods/sodium/mixin/workarounds/nvidia/MixinMinecraftClient.java
@@ -4,15 +4,13 @@ import me.jellysquid.mods.sodium.client.SodiumClientMod;
 import net.minecraft.client.MinecraftClient;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(MinecraftClient.class)
 public class MixinMinecraftClient {
-    @Inject(method = "getWindowTitle", at = @At("HEAD"), cancellable = true)
-    private void overrideWindowTitle(CallbackInfoReturnable<String> ci) {
-        if (SodiumClientMod.options().workarounds.issue1486_hideWindowTitleToEvadeNvidiaDrivers) {
-            ci.setReturnValue("Minecraft* (version hidden from driver)");
-        }
+    @ModifyArg(method = "getWindowTitle", at = @At(value = "INVOKE", target = "Ljava/lang/StringBuilder;<init>(Ljava/lang/String;)V"))
+    private String overrideWindowTitle(String str) {
+        return "Mіnec\u200Eraft";
     }
 }
