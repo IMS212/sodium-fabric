cbd4ae8d46615579ecd5c07a84e327dd8a88b218
IMS212 <ims@irisshaders.dev>
2024-04-02 17:56:56 -0700

    Potat

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 object Constants {
     // https://fabricmc.net/develop/
-    const val MINECRAFT_VERSION: String = "24w13a"
-    const val FABRIC_LOADER_VERSION: String = "0.15.7"
-    const val FABRIC_API_VERSION: String = "0.96.12+1.20.5"
+    const val MINECRAFT_VERSION: String = "24w14potato"
+    const val FABRIC_LOADER_VERSION: String = "0.15.8"
+    const val FABRIC_API_VERSION: String = "0.96.14+24w14potato"
 
     // https://semver.org/
     const val MOD_VERSION: String = "0.5.8"
