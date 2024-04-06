0c621b9d0b2e2a5ebcdb3adf904c970ddc62bc58
IMS212 <ims@irisshaders.dev>
2024-04-05 21:03:30 -0700

    Proper Forge (test 1)

--- neoforge/build.gradle.kts
+++ neoforge/build.gradle.kts
@@ -38,6 +38,27 @@ if (file("src/main/resources/META-INF/accesstransformer.cfg").exists()) {
     minecraft.accessTransformers.file("src/main/resources/META-INF/accesstransformer.cfg")
 }
 
+val fullJar: Jar by tasks.creating(Jar::class) {
+    from(sourceSets.getByName("service").output)
+    from(project(":common").sourceSets.getByName("desktop").output)
+    // Despite not being part of jarjar metadata, the mod jar must be located in this directory
+    // in order to be deobfuscated by FG in userdev environments
+    into("META-INF/jarjar/") {
+        from(tasks.jar)
+    }
+    manifest {
+        from(tasks.jar.get().manifest)
+    }
+
+    into("META-INF") {
+       // from(projectDir.resolve("src").resolve("main").resolve("resources").resolve("META-INF").resolve("mods.toml"))
+    }
+
+    manifest.attributes["Main-Class"] = "net.caffeinemc.mods.sodium.desktop.LaunchWarn"
+    manifest.attributes["FMLModType"] = "LIBRARY"
+
+}
+
 runs {
     configureEach {
         modSource(project.sourceSets.main.get())
@@ -66,12 +87,21 @@ sourceSets.main.get().resources { srcDir("src/generated/resources") }
 dependencies {
     implementation("net.neoforged:neoforge:${NEOFORGE_VERSION}")
     compileOnly(project(":common"))
-    implementation(files("fabric_renderer_api_v1-1.0.0.jar"))
+    implementation(files("fabric_renderer_api_v1-3.2.1.jar"))
+    implementation(files("fabric_api_base-0.4.31.jar"))
     implementation(group = "com.lodborg", name = "interval-tree", version = "1.0.0")
 }
 
 // NeoGradle implementations the game, but we don"t want to add our common code to the game"s code
-val notNeoTask: (Task) -> Boolean = { it : Task -> !it.name.startsWith("neo") }
+val notNeoTask: (Task) -> Boolean = { it : Task -> !it.name.startsWith("neo") && !it.name.startsWith("compileService") }
+
+tasks.jar {
+    archiveClassifier = "mod"
+}
+
+tasks.build {
+    dependsOn(fullJar)
+}
 
 tasks.withType<JavaCompile>().matching(notNeoTask).configureEach {
     source(project(":common").sourceSets.main.get().allSource)
